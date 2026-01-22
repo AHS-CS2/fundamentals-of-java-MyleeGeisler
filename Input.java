8b756006ed2d21064446f1f33a396ee1ec890cd4
MYLEE GEISLER <228235@andrews.esc18.net>
2026-01-22 11:11:57 -0600

    First commit on 'main.' Completed Java assignments 'AsciiBox.java' and 'AsciiBox2.java'.

--- Input.java
+++ Input.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Mylee Geisler
+//Date - 1 - 22 - 26
+//Class - Computer Science I
+//Lab  - Define, input, and print some variables and their values.
 
 import static java.lang.System.*;
 import java.util.Scanner;
@@ -30,6 +30,27 @@ public class Input
 
 
 		//add in input for all variables
+		//System.out.print("Enter a double :: ");
+		//doubleOne = keyboard.nextInt();
+		
+		//System.out.print("Enter a double :: ");
+		//doubleTwo = keyboard.nextInt();
+
+
+		//System.out.print("Enter a float :: ");
+		//floatOne = keyboard.nextInt();
+
+		//System.out.print("Enter a float :: ");
+		//floatTwo = keyboard.nextInt();
+
+
+		//System.out.print("Enter a short :: ");
+		//shortOne = keyboard.nextInt();
+
+		//System.out.print("Enter a short :: ");
+		//shortTwo = keyboard.nextInt();
+
+
 
 
 		System.out.println();
@@ -38,6 +59,13 @@ public class Input
 
 		//add in output for all variables
 
+		//System.out.println("double one = " + doubleOne );
+		//System.out.println("double two = " + doubleTwo );
 
+		//System.out.println("float one = " + floatOne );
+		//System.out.println("float two = " + floatTwo );
+
+		//System.out.println("short one = " + shortOne );
+		//System.out.println("short two = " + shortTwo );
 	}
 }
