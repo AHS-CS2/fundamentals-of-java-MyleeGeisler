e22189af414efc99f30d34d64408227de9e31e8f
MYLEE GEISLER <228235@andrews.esc18.net>
2026-02-02 11:14:18 -0600

    Almost done with variables lab.

--- Variables.java
+++ Variables.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Mylee Geisler
+//Date - 1 - 30 - 26
+//Class - Computer Science I
+//Lab  - Variables.java
 
 public class Variables
 {
@@ -16,17 +16,23 @@ public class Variables
 		//float		double
 		//char      boolean		String
 
+
 		//integer variables
 		byte byteOne = 127;
-
+		short shortOne = 64;
+		int intOne = 200;
+		long longOne = 2;
 
 		//decimal variables
-
+		float floatOne = 255;
+		double doubleOne = 20.5;
 
 		//other integer types
-
+		char characterOne = 65;
 
 		//other types
+		boolean booleanOne = true;
+		string stringOne = "string";
 
 
 		//output your information here
@@ -34,7 +40,22 @@ public class Variables
 		System.out.println("*                               *");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
-		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*8 bit - byteOne = " + byteOne + "\t\t*");
+		System.out.println("*16 bit - shortOne = " + shortOne + "\t\t*");
+		System.out.println("*32 bit - intOne = " + intOne + "\t\t*");
+		System.out.println("*64 bit - longOne = " + longOne + "\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*		  real types            *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = " + floatOne + "\t\t*");
+		System.out.println("*64 bit - doubleOne = " + doubleOne + "\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = " + charOne + "\t\t*");
+		System.out.println("*                               *");
+		System.out.println("")
+
 
 
 
